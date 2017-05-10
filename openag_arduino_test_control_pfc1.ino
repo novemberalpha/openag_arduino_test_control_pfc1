673e3aa8451c0b8cd1285ac08641322b8756f759
Joseph Juhnke <joe@novemberkiloecho.com>
2017-05-10 11:16:53 -0500

    refactor to switch statement

--- openag_arduino_test_control_pfc1.ino
+++ openag_arduino_test_control_pfc1.ino
@@ -1,7 +1,11 @@
 /*
-Adafruit Arduino - Lesson 05. Serial Monitor
+PFC1 Hardware Test Sketch for Arduino Mega2560 with both daugherboards
+Written by Joseph Juhnke
+License: GPL 3.0
+Written without cleverness so it's easy to mod!
 */
 
+// Declare Pins
 int heatPin = 6;
 int h2oCirculationPin = 7;
 int lightsPin = 8;
@@ -12,6 +16,7 @@ int circFanPin = 15;
 
 void setup() 
 {
+//   Initialize Pins
   pinMode(heatPin, OUTPUT);
   pinMode(h2oCirculationPin, OUTPUT);
   pinMode(lightsPin, OUTPUT);
@@ -19,9 +24,12 @@ void setup()
   pinMode(ventFanPin, OUTPUT);
   pinMode(circFanPin, OUTPUT);
 
+//   Initialize Serial
   Serial.begin(9600);
   while (! Serial); // Wait untilSerial is ready
   Serial.println("Enter Number 0 to 5 or 'x' to clear");
+  
+//   Set all pins hot
   digitalWrite(heatPin, HIGH);
   digitalWrite(h2oCirculationPin, HIGH);
   digitalWrite(lightsPin, HIGH);
@@ -37,39 +45,39 @@ void loop()
   {
     char ch = Serial.read();
     Serial.println(ch);
-    if (ch == '0')
-    {
+
+    switch(ch) {
+    case 0:
       digitalWrite(heatPin, LOW);
       Serial.println("Heater ON!");
-    }
-    if (ch == '1')
-    {
+      break;
+    case 1:
+      _serial_port = &Serial2;
       digitalWrite(h2oCirculationPin, LOW);
       Serial.println("Circulation Pump ON!");
-    }
-    if (ch == '2')
-    {
+      break;
+    case 2:
+      _serial_port = &Serial3;
       digitalWrite(lightsPin, LOW);
       Serial.println("Grow Lights ON!");
-    }
-    if (ch == '3')
-    {
+      break;
+    case 3:
+      _serial_port = &Serial3;
       digitalWrite(humidityPin, LOW);
       Serial.println("Humidifier ON!");
-    }
-    if (ch == '4')
-    {
+      break;
+    case 4:
+      _serial_port = &Serial3;
       digitalWrite(ventFanPin, LOW);
       Serial.println("Vent Fan ON!");
-    }
-    if (ch == '5')
-    {
+      break;
+    case 5:
+      _serial_port = &Serial3;
       digitalWrite(circFanPin, LOW);
       Serial.println("Circulation Fan ON!");
-    }
-    
-    if (ch == 'x')
-    {
+      break;
+    case x:
+      _serial_port = &Serial3;
       digitalWrite(heatPin, HIGH);
       digitalWrite(h2oCirculationPin, HIGH);
       digitalWrite(lightsPin, HIGH);
@@ -77,6 +85,7 @@ void loop()
       digitalWrite(ventFanPin, HIGH);  
       digitalWrite(circFanPin, HIGH);
       Serial.println("EVERYTHING OFF!");
+      break;
     }
   }
 }
